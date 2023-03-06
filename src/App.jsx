e76c10e90d8a7b6b786917abe685f8d3bfe65096
Amit chauhan <amitpoly2020@gmail.com>
2023-03-06 14:21:30 +0530

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App(){
             <BrowserRouter>
                 <Routes>
               
-                    <Route path="/" element={<ControlPannel/>}/>
+                    <Route path="/todu_React_app" element={<ControlPannel/>}/>
                     {auth?  <Route path="task" element={<Tasks/>}/>:<Route path="/" element={<ControlPannel/>}/> }
                    
                 </Routes>
